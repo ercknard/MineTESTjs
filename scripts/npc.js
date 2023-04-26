83255c6b8800e8460123d30639404bf5d2cfd067
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-04-26 17:00:40 +0800

    pepe

---
+++ scripts/npc.js
@@ -0,0 +1,20 @@
+function talk() {
+    var npcText = document.getElementById("npc-text");
+    var messages = [
+      "Hello, adventurer!",
+      "I'm Pepe.",
+      "Welcome to CrytechTest.",
+      "Wanna play with me?",
+      "Come back soon!"
+    ];
+    var index = 0;
+    setInterval(function() {
+      npcText.innerText = messages[index];
+      index++;
+      if (index >= messages.length) {
+        index = 0;
+      }
+    }, 3000);
+  }
+  
+  talk();
