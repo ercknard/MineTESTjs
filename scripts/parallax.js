0f72f6ae8bebe75e161f4f3d809907ae5712e0ef
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-04-27 17:17:09 +0800

    Update parallax.js

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,6 +1,6 @@
 let front_bg = document.getElementById('front-bg');
 let mid_bg = document.getElementById('mid-bg');
-//  let sun = document.getElementById('sun');
+ let sun = document.getElementById('sun');
 let cloudI = document.getElementById('cloudI');
 let back_bg = document.getElementById('back-bg');
 
@@ -8,7 +8,7 @@ window.addEventListener('scroll',function(){
     let value = window.scrollY;
     front_bg.style.top = value * 0.20 + 'px';
     mid_bg.style.top = value * 0.30 + 'px';
-  //   sun.style.top = value * .5 + 'px';
+    sun.style.top = value * .4 + 'px';
     cloudI.style.left = value * .5 + 'px';
     back_bg.style.top = value * 0.40 + 'px';
 })
