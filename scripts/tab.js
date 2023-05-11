7773b8fbabd2657c6d03496f59821356a8c431cc
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-05-11 09:34:48 +0800

    12

--- scripts/tab.js
+++ scripts/tab.js
@@ -54,3 +54,69 @@ btnsleads[i].addEventListener("click", function() {
   this.className += " active-leads";
 });
 }
+
+var slideIndexcoins = 0;
+carouselcoins();
+
+function carouselcoins() {
+    var i;
+    var x = document.getElementsByClassName("mySlides-coin");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+  }
+  
+  // Next/previous controls
+  function plusSlidescoins(n) {
+    showSlidecoins(slideIndexcoins += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlidecoins(n) {
+    showSlidecoins(slideIndexcoins = n);
+  }
+  
+  function showSlidecoins(n) {
+    autoScroll = false;
+    var i;
+    var slides = document.getElementsByClassName("mySlides-coin");
+    if (n > slides.length) {slideIndexcoins = 1}
+    if (n < 1) {slideIndexcoins = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndexcoins-1].style.display = "block";
+  }
+
+  function copy(el) {
+    var address = el.innerHTML;
+    if (address.length == 0) return;
+    var tmp = document.createElement("textarea");
+    tmp.setAttribute("type", "hidden");
+    tmp.value = address;
+    document.body.appendChild(tmp);
+    tmp.select();
+    document.execCommand("copy");
+    document.body.removeChild(tmp);
+    alert("Copied: " + tmp.value);
+  }
+
+  var btnContainercoin = document.getElementById("coin-active-btn");
+
+
+var btnscoin = btnContainercoin.getElementsByClassName("coin-buttons");
+
+
+for (var i = 0; i < btnscoin.length; i++) {
+  btnscoin[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-coin");
+
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active-coin", "");
+    }
+
+
+    this.className += " active-coin";
+  });
+}
